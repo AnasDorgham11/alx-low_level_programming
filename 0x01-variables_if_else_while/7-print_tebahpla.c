3daa16e69f233dcb314496a03e99462e7e06329c
AnasDorgham11 <dorghamprogrammer27@gmail.com>
2023-07-15 03:52:14 +0300

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - a to z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
+}
