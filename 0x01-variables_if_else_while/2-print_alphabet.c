fc50da895bc73214952b53a8e60733773f19cf89
AnasDorgham11 <dorghamprogrammer27@gmail.com>
2023-07-15 03:01:47 +0300

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,10 +12,10 @@
 int main(void)
 {
 	char i;
-	for (i= 'a'; i<= 'z'; i++)
-    {
-        putchar(i);
-    }
-	putchar('\n');
-	return (0);
+	for (i = 'a'; i <= 'z'; i++)
+	{    
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
 }
